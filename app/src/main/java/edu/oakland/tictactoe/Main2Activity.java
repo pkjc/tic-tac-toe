17a0b10329fb4b0eb881307b14b7fb61bcdefd93
Pankaj <mepankaj@live.in>
2018-10-23 16:08:41 -0400

    final changes for 1st 2 screens

--- app/src/main/java/edu/oakland/tictactoe/Main2Activity.java
+++ app/src/main/java/edu/oakland/tictactoe/Main2Activity.java
@@ -3,10 +3,18 @@ package edu.oakland.tictactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private EditText pl2Name;
+    private Button submitBtn;
+    private RadioGroup pl2RadioGroup;
+    private RadioButton pl2RadioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,10 +22,36 @@ public class Main2Activity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        String player1name = intent.getStringExtra("player1name");
-        String player1symbol = intent.getStringExtra("player1symbol");
+        final String player1name = intent.getStringExtra("player1name");
+        final String player1symbol = intent.getStringExtra("player1symbol");
 
         Toast.makeText(Main2Activity.this, player1name, Toast.LENGTH_SHORT).show();
         Toast.makeText(Main2Activity.this, player1symbol, Toast.LENGTH_LONG).show();
+
+        submitBtn = findViewById(R.id.player1NameSubmit);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get selected radio button from radioGroup
+                int selectedId = pl2RadioGroup.getCheckedRadioButtonId();
+                String selectedSym = null;
+                pl2Name = findViewById(R.id.pl1Name);
+
+                if(selectedId == R.id.radio1){
+                    selectedSym = "o";
+                }else {
+                    selectedSym = "x";
+                }
+
+                //TODO
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                intent.putExtra("player1name", player1name);
+                intent.putExtra("player1symbol", player1symbol);
+                intent.putExtra("player2name", pl2Name.getText().toString());
+                intent.putExtra("player2symbol", selectedSym);
+
+                startActivity(intent);
+            }
+        });
     }
 }
